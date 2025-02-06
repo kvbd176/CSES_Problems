d432cb122c33f4589b3510637c3612765d4d4098
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2025-02-06 20:55:39 +0530

    Create 2knights.cpp

---
+++ Introductory_problems/2knights.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t>0){
+long int x;
+long int y;
+cin>>x>>y;
+long int i,j,result=1;
+k=0;
+for(j=k;j>=0;j--){
+for(k=0;k<y;k++){
+result++;
+}
+}
+t--;
+}
+}
