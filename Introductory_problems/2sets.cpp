fc0814d7c56d97a4286c855a17f4731f4b4298e8
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2025-02-06 21:08:26 +0530

    Update 2sets.cpp

--- Introductory_problems/2sets.cpp
+++ Introductory_problems/2sets.cpp
@@ -1,11 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-long int n,k,result;
+long int n,i;
 cin>>n;
-for(k=1;k<=n;k++){
-result=((k*k)*((k*k)-1))/2;
-result=result-(4*(k-1)*(k-2));
-cout<<result<<endl;
+long int sum=(n*(n+1))/2;
+if(sum%2!=0){
+cout<<"NO";
+return 0;
 }
+cout<<"YES"<<endl;
+vector<long int> set1,set2;
+sum/=2;
+for(i=n;i>0;i--){
+if(sum-i>=0){
+set1.push_back(i);
+sum-=i;
+}
+else set2.push_back(i);
+}
+cout<<set1.size()<<endl;
+for(long int i: set1) cout<<i<<" ";
+cout<<endl;
+cout<<set2.size()<<endl;
+for(long int i: set2) cout<<i<<" ";
+cout<<endl;
 }
